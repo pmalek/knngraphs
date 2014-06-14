f6570fb28df6a5f7edd2b03d2b6344673ba1bc68
Patryk Małek <malekpatryk@gmail.com>
2014-06-14 13:35:46 +0200

    Added more generic parameters for Plot class allowing creating multiple dataseries in one plot easier.

--- src/main/java/pl/wroc/pwr/patryk/GraphCalculations.java
+++ src/main/java/pl/wroc/pwr/patryk/GraphCalculations.java
@@ -11,94 +11,98 @@ import edu.uci.ics.jung.graph.DirectedSparseGraph;
 
 public class GraphCalculations implements Runnable {
 
-	private String filepath;
-	private String filename;
-	private int k;
-	private boolean visualizations;
-	private boolean saveGraphImage;
-	private Thread thread;
-	
-	public GraphCalculations(String filepath, int k, boolean visualizations,
-			boolean saveGraphImage) {
-		this.filepath = filepath;
-		this.filename = Paths.get(filepath).getFileName().toString();
-		this.k = k;
-		this.visualizations = visualizations;
-		this.saveGraphImage = saveGraphImage;
-		this.thread = new Thread(this, filepath);
-	}
+    private String filepath;
+    private String filename;
+    private int k;
+    private boolean visualizations;
+    private boolean saveGraphImage;
+    private Thread thread;
 
-	@Override
-	public void run() {
-		System.out.println("\n************************************");
-		System.out.println("filepath \"" + filepath + "\"");
-		System.out.println();
-		
-		List<Double> pearsonsValues = new ArrayList<Double>();
-		List<Integer> x = new ArrayList<Integer>();
+    public GraphCalculations(
+            String filepath,
+            int k,
+            boolean visualizations,
+            boolean saveGraphImage) {
+        this.filepath = filepath;
+        this.filename = Paths.get(filepath).getFileName().toString();
+        this.k = k;
+        this.visualizations = visualizations;
+        this.saveGraphImage = saveGraphImage;
+        this.thread = new Thread(this, filepath);
+    }
 
-		long startTime = System.currentTimeMillis();
-		int totalKMaxInDegree = 0;
-		DescriptiveStatistics totalInDegreeStats = new DescriptiveStatistics();
-		DescriptiveStatistics skewnessStats = new DescriptiveStatistics();
-		totalInDegreeStats.addValue(0); // without this totalInDegreeStats.getMax() is NaN
-		List<Double> coefsOfVariation = new ArrayList<Double>();
-		List<Double> standardDeviationList = new ArrayList<Double>();
-		List<Double> maxIndegreeList = new ArrayList<Double>();
+    @Override
+    public void run() {
+        System.out.println("\n************************************");
+        System.out.println("filepath \"" + filepath + "\"");
+        System.out.println();
 
-		for (int i = 1; i <= k; i++) {
-			DescriptiveStatistics inDegreeStats = new DescriptiveStatistics();
+        ArrayList<Double> pearsonsValues = new ArrayList<Double>();
+        List<Integer> x = new ArrayList<Integer>();
 
-			MyDirectedGraph graph = new MyDirectedGraph(i, filepath);
-			graph.loadFile();
+        long startTime = System.currentTimeMillis();
+        int totalKMaxInDegree = 0;
+        DescriptiveStatistics totalInDegreeStats = new DescriptiveStatistics();
+        DescriptiveStatistics skewnessStats = new DescriptiveStatistics();
+        totalInDegreeStats.addValue(0); // without this
+                                        // totalInDegreeStats.getMax() is NaN
+        ArrayList<Double> coefsOfVariation = new ArrayList<Double>();
+        ArrayList<Double> standardDeviationList = new ArrayList<Double>();
+        ArrayList<Double> maxIndegreeList = new ArrayList<Double>();
 
-			// graph.printGraph();
-			System.out.println("\nk = " + i);
-			// graph.printLinks();
-			System.out.println("Vertices count: \""	+ graph.getVerticesCount() + "\"");
-			System.out.println("Dimension: \"" + graph.getDimension() + "\"");
-			System.out.println("Edge count: \"" + graph.getEdgeCount() + "\"");
-			double pearsonsCorrelation = graph.getPearsonsCorrelation();
-			System.out.println("PearsonsCorrelation: \"" + pearsonsCorrelation + "\"");
-			pearsonsValues.add(pearsonsCorrelation);
-			
-			int currentInDegree = 0;
-			// check inDegree of all the nodes
-			for (Iterator<Node> it = graph.getGraph().getVertices().iterator(); it.hasNext();) {
-				Node node = (Node) it.next();
-				DirectedSparseGraph<Node, Link> dirGraph = graph.getGraph();
-				currentInDegree = dirGraph.inDegree(node);
-				
-				inDegreeStats.addValue(currentInDegree);
-			}
-			
-			// X values for plot drawing
-			x.add(i);
-			
-			//stats added to containers
-			double standardDeviation = inDegreeStats.getStandardDeviation();
-			double coefficientOfVariation = standardDeviation/inDegreeStats.getMean();
-			coefsOfVariation.add(coefficientOfVariation);
-			skewnessStats.addValue(inDegreeStats.getSkewness());
+        for (int i = 1; i <= k; i++) {
+            DescriptiveStatistics inDegreeStats = new DescriptiveStatistics();
 
-			if (visualizations) {
-				new Visualize(graph.getGraph(), "k = " + i
-						+ ",pearsonsCorrelation = " + pearsonsCorrelation);
-			}
-			
-			maxIndegreeList.add(inDegreeStats.getMax());
-			standardDeviationList.add(standardDeviation);
-			
-			// stats
-			if( inDegreeStats.getMax() > totalInDegreeStats.getMax() ){
-				totalInDegreeStats.addValue(inDegreeStats.getMax());
-				totalKMaxInDegree = i;
-			}
-			System.out.println("Max in-degree="+inDegreeStats.getMax());
-			System.out.println("Average in-degree: " + inDegreeStats.getMean());
-			System.out.println("In-degree standard deviation: " + inDegreeStats.getStandardDeviation());
-			System.out.println("Coefficient of variation: " + coefficientOfVariation);
-			
+            MyDirectedGraph graph = new MyDirectedGraph(i, filepath);
+            graph.loadFile();
+
+            // graph.printGraph();
+            System.out.println("\nk = " + i);
+            // graph.printLinks();
+            System.out.println("Vertices count: \"" + graph.getVerticesCount() + "\"");
+            System.out.println("Dimension: \"" + graph.getDimension() + "\"");
+            System.out.println("Edge count: \"" + graph.getEdgeCount() + "\"");
+            double pearsonsCorrelation = graph.getPearsonsCorrelation();
+            System.out.println("PearsonsCorrelation: \"" + pearsonsCorrelation + "\"");
+            pearsonsValues.add(pearsonsCorrelation);
+
+            int currentInDegree = 0;
+            // check inDegree of all the nodes
+            for (Iterator<Node> it = graph.getGraph().getVertices().iterator(); it.hasNext();) {
+                Node node = (Node) it.next();
+                DirectedSparseGraph<Node, Link> dirGraph = graph.getGraph();
+                currentInDegree = dirGraph.inDegree(node);
+
+                inDegreeStats.addValue(currentInDegree);
+            }
+
+            // X values for plot drawing
+            x.add(i);
+
+            // stats added to containers
+            double standardDeviation = inDegreeStats.getStandardDeviation();
+            double coefficientOfVariation = standardDeviation / inDegreeStats.getMean();
+            coefsOfVariation.add(coefficientOfVariation);
+            skewnessStats.addValue(inDegreeStats.getSkewness());
+
+            if (visualizations) {
+                new Visualize(graph.getGraph(), "k = " + i
+                        + ",pearsonsCorrelation = " + pearsonsCorrelation);
+            }
+
+            maxIndegreeList.add(inDegreeStats.getMax());
+            standardDeviationList.add(standardDeviation);
+
+            // stats
+            if (inDegreeStats.getMax() > totalInDegreeStats.getMax()) {
+                totalInDegreeStats.addValue(inDegreeStats.getMax());
+                totalKMaxInDegree = i;
+            }
+            System.out.println("Max in-degree=" + inDegreeStats.getMax());
+            System.out.println("Average in-degree: " + inDegreeStats.getMean());
+            System.out.println("In-degree standard deviation: " + inDegreeStats.getStandardDeviation());
+            System.out.println("Coefficient of variation: " + coefficientOfVariation);
+
 /*			Printing all in degrees for each k
 			String allInDegrees = "";
 			for (Iterator<Integer> it = inDegrees.iterator(); it.hasNext();) {
@@ -108,53 +112,82 @@ public class GraphCalculations implements Runnable {
 			System.out.println(allInDegrees);*/
 		}
 
-		long endTime = System.currentTimeMillis();
-		System.out.println("\n\nFile load + graphs creation: " + (endTime - startTime) / 1000f + "s.");
-		
-		System.out.println("\nMax in-degree for k=" + totalKMaxInDegree + ", max_in-degree=" + totalInDegreeStats.getMax());
-		System.out.println();
-		
-		Plot plot = new Plot(
-				filename + ", Pearson coefficient",
-				"K", 
-				"Pearson correlation coefficient",
-				"Pearson correlation coefficient",
-				"Coefficient of variation");
-		plot.showGraph(x, pearsonsValues, coefsOfVariation, saveGraphImage,0);
-		
+        long endTime = System.currentTimeMillis();
+        System.out.println("\n\nFile load + graphs creation: " + (endTime - startTime) / 1000f + "s.");
+        System.out.println("\nMax in-degree for k=" + totalKMaxInDegree + ", max_in-degree=" + totalInDegreeStats.getMax());
+        System.out.println();
+
+        ArrayList<Double> skewnessList = new ArrayList<Double>();
+        for (double d : skewnessStats.getValues()) {
+            skewnessList.add(d);
+        }
+
+        List<String> mainPlotLabels = new ArrayList<String>();
+        mainPlotLabels.add("Pearson correlation coefficient");
+        mainPlotLabels.add("Coefficient of variation");
+        mainPlotLabels.add("Skewness of in-degree of graph nodes");
+
+        List<ArrayList<Double>> mainPlotDataSeries = new ArrayList<ArrayList<Double>>();
+        mainPlotDataSeries.add(pearsonsValues);
+        mainPlotDataSeries.add(coefsOfVariation);
+        mainPlotDataSeries.add(skewnessList);
+
+        double maxYMainPlot = 0.0;
+        for (ArrayList<Double> arrayList : mainPlotDataSeries) {
+            for (Double d : arrayList) {
+                if(d > maxYMainPlot)
+                    maxYMainPlot = d;
+            }
+        }
+
+        Plot plot = new Plot(
+                filename + ", Pearson coefficient",
+                "K",
+                "Pearson correlation coefficient",
+                mainPlotLabels);
+        plot.showGraph(x, mainPlotDataSeries, maxYMainPlot, saveGraphImage, 0);
+
+/*        List<String> stddevPlotLabels = new ArrayList<String>();
+        stddevPlotLabels.add("Standard deviation of in-degree");
+
+        List<ArrayList<Double>> stddevPlotDataSeries = new ArrayList<ArrayList<Double>>();
+		stddevPlotDataSeries.add(standardDeviationList);
+
 		Plot standardDeviationOfInDegreePlot = new Plot(
 				filename + ", std deviation",
 				"K",
 				"Standard deviation of in-degree",
-				"Standard deviation of in-degree",
-				null);
-		standardDeviationOfInDegreePlot.showGraph(x, standardDeviationList, null, false,0);
-		
+				stddevPlotLabels);
+		standardDeviationOfInDegreePlot.showGraph(x, stddevPlotDataSeries, false,0);
+		*/
+
+        /*List<String> maxInDegreePlotLabels = new ArrayList<String>();
+        maxInDegreePlotLabels.add("Max in-degree");
+
+        List<ArrayList<Double>> maxInDegreePlotDataSeries = new ArrayList<ArrayList<Double>>();
+        maxInDegreePlotDataSeries.add(maxIndegreeList);
+
 		Plot maxInDegreePlot = new Plot(
 				filename + ", Max in-degree",
 				"K",
 				"Max in-degree",
-				"Max in-degree",
-				null);
-		maxInDegreePlot.showGraph(x, maxIndegreeList, null, false,0);
-		
-		Plot skewnessPlot = new Plot(
+				maxInDegreePlotLabels);
+		maxInDegreePlot.showGraph(x, maxInDegreePlotDataSeries, false, 0);*/
+
+/*		Plot skewnessPlot = new Plot(
 				filename + ", skewness of in-degree",
 				"K",
 				"Skewness of in-degree",
 				"Skewness of in-degree",
+				null,
 				null);
 
-		List<Double> skewnessList = new ArrayList<Double>();
-		for (double d : skewnessStats.getValues()) {
-			skewnessList.add(d);
-		}
 		skewnessPlot.showGraph(
 				x, 
 				skewnessList,
-				null, false, 1);		
+				null,null, false, 1);	*/
 	}
-	
+
 	public void start(){
 		thread.start();
 	}
--- src/main/java/pl/wroc/pwr/patryk/Plot.java
+++ src/main/java/pl/wroc/pwr/patryk/Plot.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.geom.Ellipse2D;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,135 +24,92 @@ import de.erichseifert.gral.ui.InteractivePanel;
 
 public class Plot extends JFrame {
 
+    private static final Color COLOR_FIRST = new Color(0.0f, 0.3f, 1.0f, 0.3f);
+    private static final Color COLOR_SECOND = new Color(0.0f, 0.0f, 0.0f, 0.3f);
+    private static final Color COLOR_THIRD = new Color(0.0f, 0.7f, 0.0f, 0.3f);
+    private static final Color[] COLORS = {COLOR_FIRST, COLOR_SECOND, COLOR_THIRD};
+    private static final Ellipse2D.Double POINT_SHAPE = new Ellipse2D.Double(-2.5, -2.5, 7.0, 7.0);
+
     private static final long serialVersionUID = 8083971727860355817L;
     private int width = 1366;
     private int height = 768;
     private String xAxisLabel;
     private String yAxisLabel;
-    private String firstDataSeriesLabel;
-    private String secondDataSeriesLabel;
+    private List<String> dataLabelsList;
     private String filename;
 
     public Plot(String filename,
             String xAxisLabel,
             String yAxisLabel,
-            String firstDataSeriesLabel,
-            String secondDataSeriesLabel) {
+            List<String> dataLabelsList) {
         super(filename);
         this.filename = filename;
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(width - 10, height - 40);
         this.xAxisLabel = xAxisLabel;
         this.yAxisLabel = yAxisLabel;
-        this.firstDataSeriesLabel = firstDataSeriesLabel;
-        this.secondDataSeriesLabel = secondDataSeriesLabel;
+        this.dataLabelsList = dataLabelsList;
     }
 
-    @SuppressWarnings("unchecked")
     public void showGraph(
             List<Integer> x,
-            List<Double> firstDataSeries,
-            List<Double> secondDataSeries,
+            List<ArrayList<Double>> dataSeriesList,
+            double maxYValue,
             boolean saveFile,
             double xCrossPoint) {
 
         DataTable data = null;
-        if (secondDataSeries != null) {
-            data = new DataTable(Integer.class, Double.class, Double.class);
-            for (int i = 0; i < x.size(); i++) {
-                data.add(x.get(i),
-                        firstDataSeries.get(i),
-                        secondDataSeries.get(i));
+        // 1 more is for the X axis
+        data  = new DataTable(1 + dataSeriesList.size(), Double.class);
+        for (int i = 0; i < x.size(); i++) {
+            List<Double> temp = new ArrayList<Double>();
+            temp.add( new Double(x.get(i)));
+            for (List<Double> current : dataSeriesList) {
+                temp.add(current.get(i));
             }
-        } else {
-            data = new DataTable(Integer.class, Double.class);
-            for (int i = 0; i < x.size(); i++) {
-                data.add(x.get(i), firstDataSeries.get(i));
-            }
+            data.add(temp);
         }
 
-        DataSeries firstSeries = new DataSeries(firstDataSeriesLabel, data, 0,
-                1);
-        DataSeries secondSeries = null;
-        if (secondDataSeriesLabel != null)
-            secondSeries = new DataSeries(secondDataSeriesLabel, data, 0, 2);
+        List<DataSeries> currentDataSeriesList = new ArrayList<DataSeries>();
+        DataSeries[] currentDataSourceArray = new DataSeries[dataLabelsList.size()];
 
-        XYPlot plot = null;
-        if ( secondSeries != null)
-            plot = new XYPlot(firstSeries, secondSeries);
-        else
-            plot = new XYPlot(firstSeries);
+        for (int i = 0; i < dataLabelsList.size(); i++) {
+            currentDataSourceArray[i] =
+                    new DataSeries(dataLabelsList.get(i), data, 0,  i+1);
+            currentDataSeriesList.add(new DataSeries(dataLabelsList.get(i), data, 0, i+1));
+        }
+
+        XYPlot plot = new XYPlot(currentDataSourceArray);
+        //XYPlot plot = new XYPlot((DataSource[]) currentDataSeriesList.toArray());
 
         // Draw a tick mark and a grid line every 10 units along x axis
         int x_scale = 1;
         if (x.size() > 20) {
             Collections.sort(x);
-            x_scale = x.get(firstDataSeries.size() - 1) / 20;
+            x_scale = x.get(currentDataSeriesList.get(0).getRowCount() - 1) / 20;
         }
         plot.getAxisRenderer(XYPlot.AXIS_X).setTickSpacing(x_scale);
-        // Draw a tick mark and a grid line every 20 units along y axis
-        Double y_scale = 0.1;
-        List<Double> tempFirstSeries = secondDataSeries == null ? firstDataSeries : secondDataSeries;
-        // if (firstDataSeries.size() > 10){
-        if (tempFirstSeries.get(tempFirstSeries.size() - 1) > 2) {
-            Collections.sort(tempFirstSeries);
-            y_scale = (tempFirstSeries.get(tempFirstSeries.size() - 1) / 20);
-            // System.out.println(firstDataSeriesLabel);
-            // System.out.println("\ntempFirstSeries.get(tempFirstSeries.size()-1) ;"
-            // + tempFirstSeries.get(tempFirstSeries.size()-1));
-            y_scale = (double) Math.round(y_scale);
-            // DecimalFormat df = new DecimalFormat("#.##");
-            // y_scale = Double.parseDouble(df.format(y_scale));
-        }
-        plot.getAxisRenderer(XYPlot.AXIS_Y).setTickSpacing(y_scale);
 
-        LineRenderer lines1 = new DefaultLineRenderer2D();
-        lines1.setColor(new Color(0.0f, 0.3f, 1.0f, 0.3f));
-        plot.setLineRenderer(firstSeries, lines1);
+        // Draw a tick mark and a grid line every  ?????
+        BigDecimal maxYBigDecimal = new BigDecimal(maxYValue / 20);
+        int placesToRound = 1;
+        maxYBigDecimal = maxYBigDecimal.setScale(placesToRound, RoundingMode.CEILING);
+        plot.getAxisRenderer(XYPlot.AXIS_Y).setTickSpacing( maxYBigDecimal.doubleValue() );
 
-        if (secondSeries != null) {
-            LineRenderer lines2 = new DefaultLineRenderer2D();
-            lines2.setColor(new Color(0.0f, 0.0f, 0.0f, 0.3f));
-            plot.setLineRenderer(secondSeries, lines2);
-        }
+        for (int i = 0; i < currentDataSourceArray.length; i++) {
+            LineRenderer lines = new DefaultLineRenderer2D();
+            lines.setColor(COLORS[i]);
+            plot.setLineRenderer(currentDataSourceArray[i], lines);
 
-        PointRenderer points1 = new DefaultPointRenderer2D();
-        points1.setShape(new Ellipse2D.Double(-3.0, -3.0, 6.0, 6.0));
-        points1.setColor(new Color(0.0f, 0.3f, 1.0f, 0.3f));
-        plot.setPointRenderer(firstSeries, points1);
-
-        if (secondSeries != null ) {
-            PointRenderer points2 = new DefaultPointRenderer2D();
-            points2.setShape(new Ellipse2D.Double(-2.5, -2.5, 5.0, 5.0));
-            points2.setColor(new Color(0.0f, 0.0f, 0.0f, 0.3f));
-            plot.setPointRenderer(secondSeries, points2);
+            PointRenderer points = new DefaultPointRenderer2D();
+            points.setShape(POINT_SHAPE);
+            points.setColor(COLORS[i]);
+            plot.setPointRenderer(currentDataSourceArray[i], points);
         }
 
         getContentPane().add(new InteractivePanel(plot));
         // plot.getNavigator().setZoom(2f);
 
-        double smaller;
-        if (xCrossPoint != 0) {
-            Collections.sort(firstDataSeries);
-            smaller = firstDataSeries.get(0);
-            if (secondDataSeries != null) {
-                Collections.sort(secondDataSeries);
-                /*
-                 * for (int i = 0; i < firstDataSeries.size(); i++) {
-                 * System.out.println("firstDataSeries[i] " +
-                 * firstDataSeries.get(i)); } for (int i = 0; i <
-                 * secondDataSeries.size(); i++) {
-                 * System.out.println("secondDataSeries[i] " +
-                 * secondDataSeries.get(i)); }
-                 */
-                if (secondDataSeries.get(0) < smaller)
-                    smaller = secondDataSeries.get(0);
-            }
-            smaller *= 0.9;
-        } else {
-            smaller = xCrossPoint;
-        }
-        plot.getAxisRenderer(XYPlot.AXIS_X).setIntersection(smaller);
         // plot.getTitle().setText("k (" + x.get(0) + "," +
         // x.get(firstDataSeries.size()-1) + ") for " + x.size() + " graphs.");
         plot.getAxisRenderer(XYPlot.AXIS_X).setLabel(xAxisLabel);
